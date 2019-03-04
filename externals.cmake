13601713e3eb29270ebeeeb047494dd43d108ae7
Connor Griffith <grifcj@gmail.com>
2019-03-03 20:23:28 -0800

    Add some comments to externals file

--- externals.cmake
+++ externals.cmake
@@ -1,7 +1,14 @@
 include(FetchContent)
+
+# We want noisy, so we can see progress of population
 set(FETCHCONTENT_QUIET OFF CACHE BOOL "" FORCE)
+
+# Don't attempt to automatically update externals. This greatly speeds up
+# configure step and re-runs of build system. If user wants to change external,
+# then they can simply remove it, and it will be automatically populated.
 set(FETCHCONTENT_UPDATES_DISCONNECTED ON CACHE BOOL "" FORCE)
 
+# Place externals in build directory, but at a fixed location accessible to all build configs
 set(prefix ${CMAKE_CURRENT_LIST_DIR}/build/externals)
 
 FetchContent_Declare(googletest
@@ -23,9 +30,13 @@ function(MakeAvailable)
 
       string(TOUPPER ${ext} upperExt)
       if (EXISTS ${prefix}/${ext})
-         message(STATUS "External exists: ${ext}")
+         # Prevent additional downloads for every build configuration when
+         # source directory already exists.
          set(FETCHCONTENT_SOURCE_DIR_${upperExt} ${prefix}/${ext})
       else()
+         # When source directory doesn't exist, assume we have removed it.
+         # It'll be the same as the first time we run. We have to remove a few
+         # things to force FetchContent module to repopulate content.
          message(STATUS "External doesn't exist: ${ext}")
          unset(FETCHCONTENT_SOURCE_DIR_${upperExt} CACHE)
          file(GLOB globResult LIST_DIRECTORIES TRUE "${FETCHCONTENT_BASE_DIR}/${ext}*")
