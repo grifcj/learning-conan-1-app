737ac0d87ae7e853861b4185203f4bd3af4dc92d
Connor Griffith <grifcj@gmail.com>
2019-09-22 18:25:16 -0700

    Use conan for dep mgmt

---
+++ conanfile.py
@@ -0,0 +1,35 @@
+from conans import ConanFile, CMake, tools
+
+class AppConan(ConanFile):
+    name = "app"
+    version = "0.1.0"
+    license = "Beerware"
+    author = "Connor Griffith grifcj@gmail.com"
+    url = "https://github.com/grifcj/cmake-app"
+    description = "An app that punked Chuck Norris"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    generators = "cmake"
+    requires = ["logger/0.1.0@grifcj/stable", "math/0.1.0@grifcj/stable"]
+
+    def source(self):
+        self.run("git clone https://github.com/grifcj/cmake-app .")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        cmake.build(target='test')
+
+    def package(self):
+        self.copy("*.h", dst="include")
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["app"]
+
