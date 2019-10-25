89dbe8f21b9af0a3b0fb9d0c2791d6bec1400c2b
Maria Ninos <marianinos@Marias-MBP.attlocal.net>
2019-10-25 00:45:35 -0700

    Override find_package cache so find result always latest

--- conanfile.py
+++ conanfile.py
@@ -1,3 +1,4 @@
+import os
 from conans import ConanFile, CMake, python_requires
 
 base = python_requires("conanbase/1.0.0-nightly@grifcj/dev")
@@ -11,10 +12,13 @@ class AppConan(base.get_conanfile()):
         "revision": "auto"
     }
     requires = (
-            "logger/1.0.0-nightly@grifcj/dev",
             "math/1.0.0-nightly@grifcj/dev")
     build_requires = (
             "cmake_extensions/1.0.0-nightly@grifcj/dev")
-    generators = (
-            "cmake_paths",
-            "virtualrunenv")
+    generators = "cmake_paths"
+
+    def _make_cmake(self):
+        cmake = super()._make_cmake()
+        cmake.definitions["logger_DIR"] = ""
+        cmake.definitions["math_DIR"] = ""
+        return cmake
